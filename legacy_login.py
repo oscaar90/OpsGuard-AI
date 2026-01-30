e14efa01b6a6a41540e49a51c38a0139c5db7844
Oscar <oscaarsanchez90@gmail.com>
2026-01-30 09:43:50 +0100

    chore(cleanup): remove static credential artifacts to isolate logic test

--- legacy_login.py
+++
@@ -1,8 +0,0 @@
-import sqlite3
-
-def get_user(username):
-    # SQL Injection clara para que la IA se luzca
-    query = f"SELECT * FROM users WHERE name = '{username}'" 
-    conn = sqlite3.connect('db.sqlite')
-    cursor = conn.execute(query)
-    return cursor.fetchone()
